1fb6de3fae783a41cf61006039f17596e59c60de
belous.k <belous.k@fgosnuuas.ru>
2025-09-02 16:30:10 +0300

    feat: Задание №2: Пагинация для отелей

--- main.py
+++ main.py
@@ -6,6 +6,16 @@ app = FastAPI()
 hotels = [
     {"id": 1, "title": "Sochi", "name": "Сочи"},
     {"id": 2, "title": "Dubai", "name": "Дубай"},
+    {"id": 3, "title": "Moscow", "name": "Москва"},
+    {"id": 4, "title": "SaintPetersburg", "name": "Санкт-Петербург"},
+    {"id": 5, "title": "Kazan", "name": "Казань"},
+    {"id": 6, "title": "Anapa", "name": "Анапа"},
+    {"id": 7, "title": "Gelendzhik", "name": "Геленджик"},
+    {"id": 8, "title": "Yalta", "name": "Ялта"},
+    {"id": 9, "title": "Novosibirsk", "name": "Новосибирск"},
+    {"id": 10, "title": "Vladivostok", "name": "Владивосток"},
+    {"id": 11, "title": "Rostov", "name": "Ростов-на-Дону"},
+    {"id": 12, "title": "Kaliningrad", "name": "Калининград"},
 ]
 
 
@@ -30,15 +40,31 @@ def check_hotel_by_id(hotel_id: int, hotels: list[dict[str]]):
 def get_hotels(
     id: int | None = Query(None, description="Айдишник"),
     title: str | None = Query(None, description="Название отеля"),
+    page: int = Query(1),
+    per_page: int = Query(3),
 ):
+    if per_page < 1 or page < 1:
+        raise HTTPException(
+            status_code=422, detail="Negative pagination parameters are set"
+        )
     hotels_ = []
+    global hotels
     for hotel in hotels:
         if id and hotel["id"] != id:
             continue
         if title and hotel["title"] != title:
             continue
         hotels_.append(hotel)
-    return hotels_
+    count_hotels = len(hotels_)
+    if count_hotels == 0:
+        return []
+    num_rec = count_hotels if per_page > count_hotels else per_page
+    max_page = count_hotels // num_rec
+    if max_page == 1:
+        return hotels_
+    start = (page - 1) * num_rec if page <= max_page else (max_page - 1) * num_rec
+    end = page * num_rec if page <= max_page else max_page * num_rec
+    return hotels_[start:end]
 
 
 @app.put("/hotels/{hotel_id}")
