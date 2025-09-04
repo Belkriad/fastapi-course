34e2aa32b4dbeca5c35da915ef1acade323c8415
belous.k <belous.k@fgosnuuas.ru>
2025-09-04 13:21:49 +0300

    frat: Задание №3: Миграция для номеров

--- app/migrations/env.py
+++ app/migrations/env.py
@@ -10,7 +10,7 @@ from alembic import context
 from app.config import settings
 from app.database import Base
 from app.models.hotels import HotelsOrm
-
+from app.models.rooms import RoomsOrm
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
