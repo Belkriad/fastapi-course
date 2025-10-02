f9da5fcc5bfd779e8d270c9e0f99e205cb0c7afd
belous.k <belous.k@fgosnuuas.ru>
2025-10-02 14:48:02 +0300

    feat: Задание №13: Ручки на получение бронирований

--- src/api/bookings.py
+++ src/api/bookings.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Body, Query
 
-from src.api.dependencies import DBDep, UserIdDep
+from src.api.dependencies import DBDep, PaginationDep, UserIdDep
 from src.schemas.bookings import BookingAdd, BookingAddRequest
 
 router = APIRouter(prefix="/bookings", tags=["Бронирование"])
@@ -14,7 +14,26 @@ async def create_booking(
 ):
     price = await db.rooms.get_price(data.room_id)
     booking_data = BookingAdd(price=price, user_id=user_id, **data.model_dump())
-    print(booking_data)
     booking = await db.bookings.add(booking_data)
     await db.commit()
     return {"status": "OK", "data": booking}
+
+
+@router.get("")
+async def get_bookings(
+    pagination: PaginationDep,
+    db: DBDep,
+):
+    per_page = pagination.per_page or 5
+    return await db.bookings.get_all(
+        limit=per_page,
+        offset=per_page * (pagination.page - 1),
+    )
+
+
+@router.get("me")
+async def get_booking_for_user(
+    user_id: UserIdDep,
+    db: DBDep,
+):
+    return await db.bookings.get_filtered(user_id=user_id)
--- src/repo/bookings.py
+++ src/repo/bookings.py
@@ -1,3 +1,5 @@
+from sqlalchemy import select
+
 from src.models.bookings import BookingsOrm
 from src.repo.base import BaseRepository
 from src.schemas.bookings import Booking
@@ -6,3 +8,8 @@ from src.schemas.bookings import Booking
 class BookingsRepository(BaseRepository):
     model = BookingsOrm
     schema = Booking
+
+    async def get_all(self, limit, offset):
+        query = select(self.model).limit(limit).offset(offset)
+        result = await self.session.execute(query)
+        return result.scalars().all()
