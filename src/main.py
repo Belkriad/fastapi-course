6388105e77397cb91a205014887fae91b172819c
belous.k <belous.k@fgosnuuas.ru>
2025-12-18 11:26:41 +0300

    feat: add redis

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 import sys
+from contextlib import asynccontextmanager
 from pathlib import Path
 
 import uvicorn
 from fastapi import FastAPI
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
 
 sys.path.append(str(Path(__file__).parent.parent))
 
@@ -11,8 +14,18 @@ from src.api.bookings import router as router_bookings
 from src.api.facilities import router as router_facilities
 from src.api.hotels import router as router_hotels
 from src.api.rooms import router as router_rooms
+from src.init import redis_manager
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await redis_manager.connect()
+    FastAPICache.init(RedisBackend(redis_manager.redis), prefix="fastapi-cache")
+    yield
+    await redis_manager.close()
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(router_auth)
 app.include_router(router_hotels)
